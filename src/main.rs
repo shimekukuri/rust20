0d44d9371aedd53a33389a5c7835f1c7a27540c6
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-08-10 20:12:24 -0400

    -

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use sqlx::{Pool, Postgres};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
-    //TEST222
+    //TEST2222
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
 
     let pool = PgPoolOptions::new()
